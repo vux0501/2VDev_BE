afd2f5549bb70f57464114cf3c299c5cf0b593a3
vux0501 <hvudtkt@gmail.com>
2023-11-30 17:20:23 +0700

    fix api report

--- src/models/schemas/Report.schema.ts
+++ src/models/schemas/Report.schema.ts
@@ -3,6 +3,7 @@ import { ObjectId } from 'mongodb'
 interface ReportPostType {
   _id?: ObjectId
   user_id: ObjectId
+  root_id?: ObjectId | null
   post_id: ObjectId
   reason: string
   is_readed?: number
@@ -12,14 +13,16 @@ export default class ReportPost {
   _id: ObjectId
   user_id: ObjectId
   post_id: ObjectId
+  root_id?: ObjectId | null
   is_readed?: number
   reason: string
 
   created_at?: Date
-  constructor({ _id, user_id, post_id, is_readed, reason, created_at }: ReportPostType) {
+  constructor({ _id, user_id, post_id, root_id, is_readed, reason, created_at }: ReportPostType) {
     this._id = _id || new ObjectId()
     this.user_id = user_id
     this.post_id = post_id
+    this.root_id = root_id ? root_id : null
     this.is_readed = is_readed ? is_readed : 0
     this.reason = reason || ''
     this.created_at = created_at || new Date()
